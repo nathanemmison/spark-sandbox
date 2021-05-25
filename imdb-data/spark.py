3f0ccf04b434d83bd34c2319d2f7d5921d3ed69c
Nathan Emmison <emmisonnathan@gmail.com>
2021-05-25 21:03:04 +0100

    Improve python formatting

--- imdb-data/spark.py
+++ imdb-data/spark.py
@@ -17,70 +17,73 @@ conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.0')
 
 logger.info("Getting Spark Session and Context")
 spark = SparkSession.builder.config("spark.driver.extraClassPath", "postgresql-42.2.19.jar") \
-					.config(conf=conf) \
-					.master("local[1]") \
-                    .appName('test') \
-                    .getOrCreate()
+    .config(conf=conf) \
+    .master("local[1]") \
+    .appName('test') \
+    .getOrCreate()
 
 sc = spark.sparkContext
 
-def readLocal():
-	# Reading CSV files from local drive, removed for Git
-	# Uses data from https://data.police.uk/data/
-	logger.info("Reading local files")
-	global raw_content 
-	raw_content = sc.textFile("data/titles.tsv")
 
-	# Getting Count
-	#logger.info("Count of raw data")
-	#print(raw_content.count())
+def read_local():
+    # Reading CSV files from local drive, removed for Git
+    # Uses data from https://data.police.uk/data/
+    logger.info("Reading local files")
+    global raw_content
+    raw_content = sc.textFile("data/titles.tsv")
 
-def convertToTsv(x):
 
-	x = x.replace("\\N", "")
-	data = StringIO(x)
-	reader = csv.reader(data, delimiter='\t', quoting=csv.QUOTE_NONE)
-	for row in reader: 
+# Getting Count
+# logger.info("Count of raw data")
+# print(raw_content.count())
 
-		if len(row) != 9:
-			print(row)
-		else:
-			return row
+def convert_to_tsv(x):
+    x = x.replace("\\N", "")
+    data = StringIO(x)
+    reader = csv.reader(data, delimiter='\t', quoting=csv.QUOTE_NONE)
+    for row in reader:
 
-def importToPG():
-	# Converting to CSV and remove headers
-	logger.info("Converting to CSV and removing headers")
-	content = raw_content.filter(lambda x:len(x.split("\t")) == 9).map(convertToTsv)
+        if len(row) != 9:
+            print(row)
+        else:
+            return row
 
-	# Creates Data Frame, the data is all rows where the second value is not a header, schema is the opposite
-	logger.info("Creating data frame")
-	df = spark.createDataFrame(data = content.filter(lambda x:x[0]!='tconst'), schema=content.filter(lambda x:x[0]=='tconst').collect()[0])
 
-	logger.info("Writing Films to database")
-	df.filter(df.titleType == "movie").write.format("jdbc") \
-    .option("url", "jdbc:postgresql://localhost:5432/postgres") \
-    .option("dbtable", "public.films") \
-    .option("user", "postgres") \
-    .option("password", "mysecretpassword") \
-    .mode("overwrite") \
-    .save()
+def import_to_pg():
+    # Converting to CSV and remove headers
+    logger.info("Converting to CSV and removing headers")
+    content = raw_content.filter(lambda x: len(x.split("\t")) == 9).map(convert_to_tsv())
 
-	logger.info("Writing TV Series' to database")
-	df.filter(df.titleType == "tvSeries").write.format("jdbc") \
-    .option("url", "jdbc:postgresql://localhost:5432/postgres") \
-    .option("dbtable", "public.tv_series") \
-    .option("user", "postgres") \
-    .option("password", "mysecretpassword") \
-    .mode("overwrite") \
-    .save()
+    # Creates Data Frame, the data is all rows where the second value is not a header, schema is the opposite
+    logger.info("Creating data frame")
+    df = spark.createDataFrame(data=content.filter(lambda x: x[0] != 'tconst'),
+                               schema=content.filter(lambda x: x[0] == 'tconst').collect()[0])
 
-	
+    logger.info("Writing Films to database")
+    df.filter(df.titleType == "movie").write.format("jdbc") \
+        .option("url", "jdbc:postgresql://localhost:5432/postgres") \
+        .option("dbtable", "public.films") \
+        .option("user", "postgres") \
+        .option("password", "mysecretpassword") \
+        .mode("overwrite") \
+        .save()
+
+    logger.info("Writing TV Series' to database")
+    df.filter(df.titleType == "tvSeries").write.format("jdbc") \
+        .option("url", "jdbc:postgresql://localhost:5432/postgres") \
+        .option("dbtable", "public.tv_series") \
+        .option("user", "postgres") \
+        .option("password", "mysecretpassword") \
+        .mode("overwrite") \
+        .save()
+
+
 logger.info("Spark Sandbox Demo")
 
 logger.info("Getting Data")
-readLocal()
+read_local()
 
 logger.info("Import into Postgres")
-importToPG()
+import_to_pg()
 
 sc.stop()
